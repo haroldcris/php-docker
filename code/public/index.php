8e6fa89e483dd2c108c36146def1844cb2e624b2
=HaroldCris <haroldcris@aitech.solutions>
2021-02-28 14:08:07 +0800

    Added: MSSQL Driver

--- code/public/index.php
+++ code/public/index.php
@@ -1,3 +1,20 @@
 <?php
 
-phpinfo();
+return phpinfo();
+
+
+try
+{
+    $pdo = new PDO('sqlsrv:Server=192.168.100.212;Database=database', 'sa', 'password');
+
+} catch( Exception $e)
+{
+    die (print_r($e->getMessage()));
+}
+
+$statement = $pdo->prepare("SELECT Firstname FROM Employees");
+$statement->execute();
+$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+$json = json_encode($results);
+
+echo $json;
