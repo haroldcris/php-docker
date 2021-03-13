9c8537a4b435094a06281a2dd90fa9c80a81372e
=HaroldCris <haroldcris@aitech.solutions>
2021-03-13 21:44:12 +0800

    With MsSql

--- code/public/index.php
+++ code/public/index.php
@@ -1,20 +1,3 @@
 <?php
 
-return phpinfo();
-
-
-try
-{
-    $pdo = new PDO('sqlsrv:Server=192.168.100.212;Database=database', 'sa', 'password');
-
-} catch( Exception $e)
-{
-    die (print_r($e->getMessage()));
-}
-
-$statement = $pdo->prepare("SELECT Firstname FROM Employees");
-$statement->execute();
-$results = $statement->fetchAll(PDO::FETCH_ASSOC);
-$json = json_encode($results);
-
-echo $json;
+    return phpinfo();
